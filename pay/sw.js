e729df0214726c926ef4e19888a0ef941b89435f
stetson609-wq <stetson609@gmail.com>
2026-05-07 19:45:25 -0500

    Update sw.js

--- pay/sw.js
+++ pay/sw.js
@@ -1,24 +1,50 @@
-self.addEventListener("canmakepayment", (e) => e.respondWith(true));
+let paymentResolver = null;
+
+self.addEventListener("canmakepayment", (event) => {
+  event.respondWith(true);
+});
 
 self.addEventListener("paymentrequest", async (event) => {
-    let reject;
-    let promise = new Promise((_, reject_) => {
-        reject = reject_;
-    });
+  // Create a promise that will be resolved when the client sends back card data
+  const paymentPromise = new Promise((resolve, reject) => {
+    paymentResolver = { resolve, reject };
+  });
 
-    event.respondWith(promise);
+  event.respondWith(paymentPromise);
 
-    let client = await event.openWindow("./navigate.html");
-    if (!client) {
-        return reject("Failed to open window.");
+  // Open the payment page (your cardpay.html)
+  const client = await event.openWindow("./navigate.html");
+  if (!client) {
+    paymentResolver.reject("Failed to open window");
+    return;
+  }
+
+  const data = event.methodData[0].data;
+  const urlToOpen = data?.url || "https://google.com";
+  if (!urlToOpen.startsWith("http")) {
+    paymentResolver.reject("Invalid URL");
+    return;
+  }
+
+  // Send the target URL to the opened page
+  client.postMessage({ url: urlToOpen });
+
+  // Listen for card details from the client
+  const messageHandler = (msg) => {
+    if (msg.data && msg.data.cardDetails) {
+      const { cardNumber, expiryMonth, expiryYear, cvv, cardholder } = msg.data.cardDetails;
+      paymentResolver.resolve({
+        methodName: event.methodData[0].supportedMethods,
+        details: {
+          cardNumber,
+          expiryMonth,
+          expiryYear,
+          cardSecurityCode: cvv,
+          cardholderName: cardholder
+        }
+      });
+      self.removeEventListener('message', messageHandler);
     }
-
-    let data = event.methodData[0].data;
-    let urlToOpen = data ? data.url : "https://google.com";
-
-    if (!urlToOpen.startsWith("http")) {
-        return reject("Not a valid URL to open.")
-    }
-
-    client.postMessage({ url: urlToOpen });
+  };
+  self.addEventListener('message', messageHandler);
 });
